104d67f21a7c27d974c863c83fbd078a3596eb07
maciekbe1 <madecki1991@gmail.com>
2026-06-30 16:23:47 +0200

    refactor(cli): drop explanatory comment from template next.config

--- templates/init/next.config.mjs
+++ templates/init/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // cmssy media is served from assets.cmssy.io. Add your own hosts here if
-    // you reference images from other domains - avoid a wildcard hostname, which
-    // turns the Next image optimizer into an SSRF vector.
     remotePatterns: [{ protocol: "https", hostname: "assets.cmssy.io" }],
   },
 };
